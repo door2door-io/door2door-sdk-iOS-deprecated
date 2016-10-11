d6309b66525f340e96c06e786cd438d8a325cf73
Elmar Tampe <elmar@door2door.io>
2016-10-11 16:09:26 +0200

    Move umbrella header to folder for clean structure.

---
+++ D2DEventCollectionKit/Source/EventCollectionKit.swift
@@ -0,0 +1,9 @@
+//
+//  EventCollectionKit.swift
+//  D2DEventCollectionKit
+//
+//  Created by Elmar Tampe on 11/10/2016.
+//  Copyright © 2016 Door2Door GmbH. All rights reserved.
+//
+
+import Foundation
