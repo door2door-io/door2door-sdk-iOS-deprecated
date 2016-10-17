317aabc1531954593e768ae027a47312dbdc5a61
Elmar Tampe <elmar@door2door.io>
2016-10-17 14:20:58 +0200

    Change build script.

--- D2DEventCollectionKit/Scripts/test.sh
+++ D2DEventCollectionKit/Scripts/test.sh
@@ -2,4 +2,4 @@
 
 echo "Testing..."
 
-xcodebuild test D2DEventCollectionKit/D2DEventCollectionKit.xcproject -scheme D2DEventCollectionKitTests -sdk iphonesimulator10 -destination 'platform=iOS Simulator,name=iPhone 6,OS=10' ONLY_ACTIVE_ARCH=YES | xcpretty -t; exit ${PIPESTATUS[0]}
+xcodebuild test D2DEventCollectionKit/D2DEventCollectionKit.xcproject -scheme D2DEventCollectionKitTests -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.0' ONLY_ACTIVE_ARCH=YES | xcpretty -t; exit ${PIPESTATUS[0]}
