87db405002fc6824885dbdf789e8bd12bbf625aa
Elmar Tampe <elmar@door2door.io>
2016-10-17 14:12:23 +0200

    Update again for travis.

--- D2DEventCollectionKit/Scripts/test.sh
+++ D2DEventCollectionKit/Scripts/test.sh
@@ -2,4 +2,4 @@
 
 echo "Testing..."
 
-xcodebuild test D2DEventCollectionKit/D2DEventCollectionKit.xcproject -scheme D2DEventCollectionKitTests -sdk iphonesimulator9.3 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3' ONLY_ACTIVE_ARCH=YES | xcpretty -t; exit ${PIPESTATUS[0]}
+xcodebuild test D2DEventCollectionKit/D2DEventCollectionKit.xcproject -scheme D2DEventCollectionKitTests -sdk iphonesimulator9.2 -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.2' ONLY_ACTIVE_ARCH=YES | xcpretty -t; exit ${PIPESTATUS[0]}
