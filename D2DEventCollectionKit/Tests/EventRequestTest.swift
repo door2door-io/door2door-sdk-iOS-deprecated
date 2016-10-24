babe1d75d0c0bb5a16eaaf1410423a2a0fc6ed52
Elmar Tampe <elmar@door2door.io>
2016-10-24 13:35:28 +0200

    Added test request class boilerplate stuff.

---
+++ D2DEventCollectionKit/Tests/EventRequestTest.swift
@@ -0,0 +1,15 @@
+//
+//  EventRequestTest.swift
+//  D2DEventCollectionKit
+//
+//  Created by Elmar Tampe on 19/10/2016.
+//  Copyright © 2016 Door2Door GmbH. All rights reserved.
+//
+
+import XCTest
+
+@testable import D2DEventCollectionKit
+
+class EventRequestTest: XCTestCase {
+    
+}
