0edfc9e0967b709af555f5ddd9f0d66b6947d8b3
Elmar Tampe <elmar@door2door.io>
2016-10-26 13:42:47 +0200

    Mapping the agreed data model in code.

--- D2DEventCollectionKit/Source/Model/Place.swift
+++ D2DEventCollectionKit/Source/Model/Place.swift
@@ -2,13 +2,19 @@
 //  Place.swift
 //  D2DEventCollectionKit
 //
-//  Created by Elmar Tampe on 18/10/2016.
+//  Created by Elmar Tampe on 26/10/2016.
 //  Copyright © 2016 Door2Door GmbH. All rights reserved.
 //
 
 import Foundation
 
-public class Place {
-    
-    
+public struct Place {
+
+    public fileprivate(set) var latitude: Double?
+    public fileprivate(set) var longitude: Double?
+    public fileprivate(set) var name: String?
+    public fileprivate(set) var street: String?
+    public fileprivate(set) var city: String?
+    public fileprivate(set) var postalCode: String?
+    public fileprivate(set) var country: String?
 }
