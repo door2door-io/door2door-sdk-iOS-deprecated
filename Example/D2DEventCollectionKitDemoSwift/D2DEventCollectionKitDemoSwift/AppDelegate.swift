fd8c688372b1a8ab50fd4d53b1647aefa6cea6fd
Elmar Tampe <elmar@door2door.io>
2016-11-03 15:39:25 +0100

    Enable logging for the samples by default

--- Example/D2DEventCollectionKitDemoSwift/D2DEventCollectionKitDemoSwift/AppDelegate.swift
+++ Example/D2DEventCollectionKitDemoSwift/D2DEventCollectionKitDemoSwift/AppDelegate.swift
@@ -40,7 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                                     applicationName: "Ally App",
                                     applicationVersion: "1.0.0")
         
-        EventCollectionKit.enableLogging(logginEnabled: false)
+        EventCollectionKit.enableLogging(logginEnabled: true)
     }
     
     
