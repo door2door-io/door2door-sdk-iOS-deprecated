04391a733c77159d98254ae1c66d9c28c89a4f84
Elmar Tampe <elmar@door2door.io>
2016-11-11 15:58:13 +0100

    Add boilerplate code for upcoming event stages.

---
+++ D2DEventCollectionKit/Source/Events/Trip/TripEvent+Begin.swift
@@ -0,0 +1,9 @@
+//
+//  TripEvent+Begin.swift
+//  D2DEventCollectionKit
+//
+//  Created by Elmar Tampe on 11/11/2016.
+//  Copyright © 2016 Door2Door GmbH. All rights reserved.
+//
+
+import Foundation
---
+++ D2DEventCollectionKit/Source/Events/Trip/TripEvent+Book.swift
@@ -0,0 +1,9 @@
+//
+//  TripEvent+Book.swift
+//  D2DEventCollectionKit
+//
+//  Created by Elmar Tampe on 11/11/2016.
+//  Copyright © 2016 Door2Door GmbH. All rights reserved.
+//
+
+import Foundation
