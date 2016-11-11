c4e4b170eab455654a4cdffb06d1966e9ef36924
Elmar Tampe <elmar@door2door.io>
2016-11-11 16:22:32 +0100

    More events.

--- D2DEventCollectionKit/Source/Events/Trip/TripEvent+Begin.swift
+++ D2DEventCollectionKit/Source/Events/Trip/TripEvent+Begin.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+
+extension TripEvent {
+    
+    // ------------------------------------------------------------------------------------------
+    // MARK: Convenience Initializer
+    // ------------------------------------------------------------------------------------------
+    public class func tripBeginEvent(modeOfTransportations: [ModeOfTransportations],
+                                     departureTime: Date?,
+                                     originLatitude: Double,
+                                     originLongitude: Double,
+                                     originName: String?,
+                                     originStreet: String?,
+                                     originCity: String?,
+                                     originPostalCode: String?,
+                                     originCountry: String?,
+                                     arrivalTime:Date?,
+                                     destinationLatitude: Double,
+                                     destinationLongitude: Double,
+                                     destinationName: String?,
+                                     destinationStreet: String?,
+                                     destinationCity: String?,
+                                     destinationPostalCode: String?,
+                                     destinationCountry: String?) -> TripEvent {
+        
+        let trip = Trip.trip(modeOfTransportations: modeOfTransportations,
+                             departureTime: departureTime,
+                             originLatitude: originLatitude,
+                             originLongitude: originLongitude,
+                             originName: originName,
+                             originStreet: originStreet,
+                             originCity: originCity,
+                             originPostalCode: originPostalCode,
+                             originCountry: originCountry,
+                             arrivalTime:arrivalTime,
+                             destinationLatitude: destinationLatitude,
+                             destinationLongitude: destinationLongitude,
+                             destinationName: destinationName,
+                             destinationStreet: destinationStreet,
+                             destinationCity: destinationCity,
+                             destinationPostalCode: destinationPostalCode,
+                             destinationCountry: destinationCountry)
+        
+        return TripEvent(stage: .begin, trip: trip)
+    }
+}
